390941c26be1f9b03f08decb1b8a9e1245b7fe03
aighita <andreighita03@gmail.com>
2025-01-02 13:49:07 +0200

    web config with sass loader

--- webpack.config.js
+++ webpack.config.js
@@ -10,9 +10,21 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
+        test: /\.(js|jsx)$/, // Handle JS and JSX files
         exclude: /node_modules/,
         use: ['babel-loader'],
+      },
+      {
+        test: /\.scss$/, // Handle SCSS files
+        use: [
+          'style-loader', // Inject styles into DOM
+          'css-loader',   // Turn CSS into CommonJS
+          'sass-loader',  // Compile SCSS to CSS
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i, // Handle image files
+        type: 'asset/resource',
       },
     ],
   },
@@ -21,8 +33,11 @@ module.exports = {
       template: './public/index.html',
     }),
   ],
+  resolve: {
+    extensions: ['.js', '.jsx'], // Resolve JS and JSX extensions
+  },
   devServer: {
-    static: './dist',
-    hot: true,
+    static: './dist', // Serve files from the dist folder
+    hot: true,        // Enable hot module replacement
   },
 };
